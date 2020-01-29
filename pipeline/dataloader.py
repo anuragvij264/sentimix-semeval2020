abaa3a242c33809e747aaa3e8b05c84fde395d9f
Vij <avij1@LAMU02X25KLJG5M.uhc.com>
2020-01-29 12:15:26 +0530

    dataloader modified for 3 classes

--- pipeline/dataloader.py
+++ pipeline/dataloader.py
@@ -8,18 +8,32 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def custom_collate(batch):
+
+    d = {"positive":0,"negative":1,"neutral":2}
+
     data = [item[0] for item in batch]
     target = [item[1] for item in batch]
     length = [len(x) for x in data]
     data = sequence.pad_sequences(data, maxlen=max(length))
-    data = torch.tensor(data, dtype=torch.long)
-    target = torch.tensor(int(target == 'positive'), dtype=torch.float32)
-    return [data, target]
+    data = torch.tensor(data, dtype=torch.double)
+
+    target = list(map(lambda x: d[x] ,target))
+    target = torch.tensor(target, dtype=torch.float32)
+
+    _target = target.view((len(batch),1))
+
+    # target is of shape (batch_size,1)
+    return [data, _target]
 
 
 d_set = Dataset(binPath=bin_path, data_path=data_path + 'data.json')
 loader = DataLoader(dataset=d_set, batch_size=12, collate_fn=custom_collate)
 
-for idx, (batch_ndx, sample) in enumerate(loader):
-    print(sample)
-    break
+#
+# example = []
+#
+# for idx, (batch_ndx, sample) in enumerate(loader):
+#     print(batch_ndx.shape)
+#     print(sample)
+#     print(sample.size())
+#     print(sample.dtype)
