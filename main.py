429783e1a38e05b22bcac4052ec1d339e35d41d1
Vij <avij1@LAMU02X25KLJG5M.uhc.com>
2020-01-29 12:13:58 +0530

    main.py working

--- main.py
+++ main.py
@@ -6,24 +6,25 @@ from config import bin_path, data_path
 from torch.utils.data import DataLoader
 from pipeline.trans import Dataset
 from pipeline.dataloader import custom_collate
+
+from torch.nn.functional import cross_entropy
 import numpy as np
 
 n_vocab = 100000
 batch_size = 12
 n_embed = 300
 n_hidden = 10
-output_size = 2
+output_size = 3
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 net = Classifier(batch_size, n_hidden, n_vocab, n_embed, output_size)
 
-criterion = nn.BCEWithLogitsLoss()
-optimizer = optim.Adam(net.parameters(), lr=0.001)
+optimizer = optim.Adam(filter(lambda x: x.requires_grad,net.parameters()), lr=0.001)
 
 print_every = 100
 step = 0
-n_epochs = 1  # validation loss increases from ~ epoch 3 or 4
+n_epochs = 2  # validation loss increases from ~ epoch 3 or 4
 clip = 5  # for gradient clip to prevent exploding gradient problem in LSTM/RNN
 
 d_set_train = Dataset(binPath=bin_path, data_path=data_path + 'data.json')
@@ -32,34 +33,63 @@ train_loader = DataLoader(dataset=d_set_train, batch_size=12, collate_fn=custom_
 d_set_val = Dataset(binPath=bin_path, data_path=data_path + 'data.json')
 valid_loader = DataLoader(dataset=d_set_val, batch_size=12, collate_fn=custom_collate)
 
-for epoch in range(n_epochs):
+def train(model,training_data_loader,validation_data_loader):
 
-    for inputs, labels in train_loader:
-        step += 1
-        inputs, labels = inputs.to(device), labels.to(device)
-        net.zero_grad()
-        output_train = net(inputs)
-        loss = criterion(output_train.squeeze(), labels.float())
-        loss.backward()
-        nn.utils.clip_grad_norm(net.parameters(), clip)
-        optimizer.step()
 
-        if (step % print_every) == 0:
-            ######################
-            ##### VALIDATION #####
-            ######################
-            net.eval()
-            valid_losses = []
+    step = 0
+    model.train()
+    loss_total_epoch = 0
+    accuracy_total_epoch = 0
+    for epoch in range(n_epochs):
+        for inputs, target in training_data_loader:
 
-            for v_inputs, v_labels in valid_loader:
-                v_inputs, v_labels = inputs.to(device), labels.to(device)
+            inputs = inputs.float()
+            inputs, target = inputs.to(device), target.to(device)
+            model.zero_grad()
+            prediction = model(inputs)
+            # prediction is of size (batch_size,C)
+            # target is of size (batch_size)
+            loss = cross_entropy(prediction, target.squeeze().long())
 
-                v_output = net(v_inputs)
-                v_loss = criterion(v_output.squeeze(), v_labels.float())
-                valid_losses.append(v_loss.item())
+            loss_total_epoch+=loss.item()
+            loss.backward()
+            nn.utils.clip_grad_norm(model.parameters(), clip)
+            optimizer.step()
+
+            running_corrects= (torch.max(prediction, 1)[1].view(target.size()
+                                                   ).data == target.data).float().sum()
+            accuracy  = 100.0 * (running_corrects.data/inputs.size()[0])
+
+            accuracy_total_epoch+=accuracy.item()
+            step+=1
+
+        if (epoch % 2)== 0 :
+            model.eval()
+            validation_loss = 0
+            validation_accuracy = 0
+
+            for validation_inputs, validation_target in validation_data_loader:
+                validation_inputs, validation_target = validation_inputs.to(device), validation_target.to(device)
+
+                v_prediction = model(validation_inputs)
+                v_loss = cross_entropy(v_prediction, validation_target.squeeze().long())
+                validation_loss+=v_loss.item()
+
+                running_corrects_validation = (torch.max(v_prediction, 1)[1].view(validation_target.size()
+                                                                     ).data == validation_target.data).float().sum()
+                validation_accuracy = 100.0 * (running_corrects_validation.data / validation_inputs.size()[0])
+
+                validation_loss += v_loss.item()
+                validation_accuracy += validation_accuracy.item()
 
             print("Epoch: {}/{}".format((epoch + 1), n_epochs),
                   "Step: {}".format(step),
                   "Training Loss: {:.4f}".format(loss.item()),
-                  "Validation Loss: {:.4f}".format(np.mean(valid_losses)))
-            net.train()
+                  "Validation Loss: {:.4f}".format(np.mean(validation_loss)),
+                  "Validation accuracy : {: .4f}".format(np.mean(validation_accuracy)),
+                  "Training accuracy:  :{: .4f}".format(accuracy.item()),
+
+                  )
+
+if __name__=='__main__':
+    train(net,train_loader,valid_loader)
