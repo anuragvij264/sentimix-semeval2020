11e304a60096b0e7f2a3576b8c451ab6b6c5a7b4
Vij <avij1@LAMU02X25KLJG5M.uhc.com>
2020-02-12 09:55:32 +0530

    dataloader

--- pipeline/dataloader.py
+++ pipeline/dataloader.py
@@ -3,6 +3,7 @@ from pipeline.trans import Dataset
 from keras.preprocessing import sequence
 from config import bin_path, data_path
 import torch
+import os
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -22,18 +23,28 @@ def custom_collate(batch):
 
     _target = target.view((len(batch),1))
 
-    # target is of shape (batch_size,1)
-    return [data, _target]
+    #target is of shape (batch_size,1)
+    return [data, target]
 
+def load_embeddings(path, lang):
+    weights = torch.load(open(os.path.join(path, "embeddings_{}.pth".format(lang)), "rb"))
+    weights = torch.from_numpy(weights).double()
+    # embeddings = nn.Embedding.from_pretrained(weights)
+    return weights
 
-d_set = Dataset(binPath=bin_path, data_path=data_path + 'data.json')
-loader = DataLoader(dataset=d_set, batch_size=12, collate_fn=custom_collate)
+hi_embeddings = load_embeddings(path=os.path.join("/Users/avij1/Desktop/imp_shit/sec/bin", "embeddings"), lang='hi')
+en_embeddings = load_embeddings(path=os.path.join("/Users/avij1/Desktop/imp_shit/sec/bin", "embeddings"), lang='en')
+
+d_set = Dataset(binPath=bin_path, data_path=data_path + 'data.json',hi_embeddings=hi_embeddings,en_embeddings=en_embeddings)
+loader = DataLoader(dataset=d_set, batch_size=12,collate_fn=custom_collate)
 
 #
 # example = []
+# from tqdm import tqdm
 #
-# for idx, (batch_ndx, sample) in enumerate(loader):
+# for idx, (batch_ndx, sample) in tqdm(enumerate(loader)):
 #     print(batch_ndx.shape)
 #     print(sample)
-#     print(sample.size())
+# #     print(sample.size())
 #     print(sample.dtype)
+#
